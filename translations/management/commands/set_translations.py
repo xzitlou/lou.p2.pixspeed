5f57f404b15bd5f1050aa19fc70d6fd4c0b7f561
xzitlou <hola@loualcala.com>
2024-10-28 08:02:43 -0500

    fix set_translations

--- translations/management/commands/set_translations.py
+++ translations/management/commands/set_translations.py
@@ -2,6 +2,7 @@ import json
 
 from django.core.management import BaseCommand
 
+from translations.models.language import Language
 from translations.models.translation import Translation
 
 
@@ -9,14 +10,14 @@ class Command(BaseCommand):
     help = 'set_translations'
 
     def handle(self, *args, **options):
-        for lang in ["en", "es", "pt"]:
-            with open(f'./translations/json/textbase_data_{lang}.json') as translation_json:
+        for lang in Language.objects.all():
+            with open(f'./translations/json/textbase_data_{lang.iso}.json') as translation_json:
                 translations = json.load(translation_json)
 
             for translation in translations:
                 Translation.objects.update_or_create(
                     code_name=translation['code_name'],
-                    language=lang,
+                    language=lang.iso,
                     defaults={
                         "text": translation['text'],
                     }
